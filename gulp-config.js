812742bc191c75fc4464ed3ee87e75e5d783281a
Monit gupta <mongupta3@publicisgroupe.net>
2022-08-16 13:10:24 +0530

    Add project files.

---
+++ gulp-config.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    var config = {
+        webRoot: "C:\inetpub\wwwroot\sitecoretestsc.dev.local",
+        devRoot: "C:\\SitecoreShades\\SXA-Project\\SitecoreShades",
+        solutionName: "SitecoreShades",
+        buildConfiguration: "Debug",
+        MSBuildToolsVersion: 16.0
+    }
+    return config;
+}

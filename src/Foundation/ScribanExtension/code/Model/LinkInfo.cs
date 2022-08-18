ce2da668e66923a7daff74b534de70c4320aa293
Monit gupta <mongupta3@publicisgroupe.net>
2022-08-18 17:36:07 +0530

    new changes

---
+++ src/Foundation/ScribanExtension/code/Model/LinkInfo.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Data.Fields;
+
+namespace HelixSetupSolution.Project.StandBlog.Model
+{
+    public class LinkInfo
+    {
+        public string url { get; set; }
+        public string text { get; set; }
+        public string title { get; set; }
+        public string anchor { get; set; }
+        public string target { get; set; }
+
+        public LinkInfo() { }
+
+        public LinkInfo(LinkField item)
+        {
+            url = item.GetFriendlyUrl();
+            text = item.Text;
+            title = item.Title;
+            anchor = item.Anchor;
+            target = item.Target;
+        }
+    }
+}
